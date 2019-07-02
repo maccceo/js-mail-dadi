e0213807b3ad5bed07f53d6444f3e33e1aee77b3
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-02 15:42:20 +0200

    Mail - logica completata

--- mail/javascripts/main.js
+++ mail/javascripts/main.js
@@ -1,13 +1,33 @@
 // Chiedi all’utente la sua email,  controlla che sia nella lista
 // di chi può accedere e stampa un messaggio appropriato
 
+var userMail, userFound = false;
 //genero lista email accettate
-
+var registered = [
+	"maurizioaltofuoco@libero.it",
+	"grazianotiralecuoia@gmail.com",
+	"pietroturbolento@tim.it",
+	"ninoeldorado@gmail.com",
+	"denniscourmayeur@hotmail.com",
+	"lorisbavaria@hotmail.com"
+];
 
 //chiedo l'email dell'utente
-
+userMail = prompt("Inserisci la tua email");
 
 //verifico se è presente nel db
-
+for (var i = 0; i < registered.length; i++) {
+	//verifico che il ciclo for funzioni correttamente
+	console.log(registered[i]);
+	if (userMail == registered[i]) {
+		userFound = true;
+		console.log("Trovato!");
+	}
+}
 
 //stampo il risultato della ricerca
+if (userFound) {
+	document.getElementById("result").innerHTML = "Puoi entrare";
+} else {
+	document.getElementById("result").innerHTML = "Spiacente, l'indirizzo " + userMail + " non è stato trovato";
+}
