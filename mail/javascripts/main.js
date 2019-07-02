2366cc63e7630aa4dbb8aa3811751b67818007ef
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-02 15:30:07 +0200

    Mail - definizione step procedurali

---
+++ mail/javascripts/main.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente la sua email,  controlla che sia nella lista
+// di chi può accedere e stampa un messaggio appropriato
+
+//genero lista email accettate
+
+
+//chiedo l'email dell'utente
+
+
+//verifico se è presente nel db
+
+
+//stampo il risultato della ricerca
